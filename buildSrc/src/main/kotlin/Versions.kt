1d3fc48578bba3cb1cf5034669b67a73f1bc8adb
Domagoj Latečki <domagoj.latecki@gmail.com>
2022-04-07 12:54:15 +0200

    SD-710: update logging config

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion
 
 object Versions {
 
-    const val project = "0.1.0"
+    const val project = "0.1.1"
 
     object Compile {
         const val kotlin = "1.6.10"
